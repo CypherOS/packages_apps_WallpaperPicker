b30c7415b2c1ff758fa886c08afce80cf6de698b
Chris <chris@cypheros.co>
2017-08-11 15:41:09 -0400

    WallpaperPicker: add new theme for setting wallpaper
    This adds a new theme to match stock Android or Pixel Launcher,
    when setting the selected wallpaper to lockscreen, homescreen or both.
    
    Change-Id: Iab35b4df650afc38f78e690cd9115a2038fe7305

--- src/com/android/wallpaperpicker/common/DialogUtils.java
+++ src/com/android/wallpaperpicker/common/DialogUtils.java
@@ -4,6 +4,8 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.AsyncTask;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 
 import com.android.wallpaperpicker.R;
 
@@ -20,10 +22,42 @@ public class DialogUtils {
     public static void executeCropTaskAfterPrompt(
             Context context, final AsyncTask<Integer, ?, ?> cropTask,
             DialogInterface.OnCancelListener onCancelListener) {
+				
+        final Icon[] icons = {
+            new Icon(R.drawable.ic_home),
+            new Icon(R.drawable.ic_lockscreen),
+            new Icon(R.drawable.ic_both),
+        };
+				
+		ListAdapter adapter = new ArrayAdapter<Integer>(context,
+                android.R.layout.select_dialog_item,
+                android.R.id.text1, icons) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                String[] items = context.getResources().getStringArray(
+                        R.array.which_wallpaper_options);
+                float padding = context.getResources().getDimensionPixelSize(
+                        R.dimen.wallpaper_text_padding);
+                float leftPadding = context.getResources().getDimensionPixelSize(
+                        R.dimen.wallpaper_text_padding_left);
+                float textSize = context.getResources().getDimensionPixelSize(
+                        R.dimen.wallpaper_text_size);
+
+                View view = super.getView(position, convertView, parent);
+                TextView text = (TextView) view.findViewById(android.R.id.text1);
+                text.setText(items[position]);
+                text.setTextSize(textSize);
+                text.setCompoundDrawablesWithIntrinsicBounds(icons.get(position), 0, 0, 0);
+                text.setPadding((int) leftPadding, 0, 0, 0);
+                text.setCompoundDrawablePadding((int) padding);
+
+                return view;
+            }
+        };
         if (Utilities.isAtLeastN()) {
             new AlertDialog.Builder(context)
                     .setTitle(R.string.wallpaper_instructions)
-                    .setItems(R.array.which_wallpaper_options, new DialogInterface.OnClickListener() {
+                    .setAdapter(adapter, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int selectedItemIndex) {
                         int whichWallpaper;
@@ -42,6 +76,13 @@ public class DialogUtils {
                 .show();
         } else {
             cropTask.execute(WallpaperManagerCompat.FLAG_SET_SYSTEM);
+        }
+    }
+	
+	public static class Icon{
+        public final int icon;
+        public Icon(Integer icon) {
+            this.icon = icon;
         }
     }
 }
