be9c4b9332d0479d7eb70305e5ad12216e409d47
Chris <chris@cypheros.co>
2017-08-11 17:50:38 -0400

    WallpaperPicker: add new theme for setting wallpaper
    This adds a new theme to match stock Android or Pixel Launcher,
    when setting the selected wallpaper to lockscreen, homescreen or both.
    
    Referenced from: AOSPA@android_packages_apps_ParanoidPapers/96e3f36ad14c7bb259dffbb7216e55ada982294c
    Credits to: @Thecrazyskull for the original code
    
    Change-Id: Iab35b4df650afc38f78e690cd9115a2038fe7305

--- Android.mk
+++ Android.mk
@@ -20,11 +20,20 @@ LOCAL_PATH := $(call my-dir)
 # ========================================================
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    android-support-v4 \
+    android-support-v7-appcompat
+
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
+    frameworks/support/v7/appcompat/res
+
 LOCAL_SDK_VERSION := current
 LOCAL_PACKAGE_NAME := WallpaperPicker
+
+LOCAL_AAPT_FLAGS := --auto-add-overlay \
+    --extra-packages android.support.v7.appcompat
+
 include $(BUILD_PACKAGE)
 
 
